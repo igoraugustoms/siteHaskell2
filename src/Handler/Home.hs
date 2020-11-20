bde13d8f61ebb2ab07c8c7689d113e32dff2d553
Igor Augusto Monteiro Silva <igoraugustoms91@gmail.com>
2020-11-19 21:27:54 -0300

    alteracao da home

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,31 +9,21 @@ module Handler.Home where
 import Import
 import Database.Persist.Postgresql
 
---         <img src=@{StaticR img_produto_jpg}>
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do 
     addStylesheet (StaticR css_bootstrap_css)
     sess <- lookupSession "_EMAIL"
     [whamlet|
+
         <h1>
-            SISTEMA DE PRODUTO
+            LIVRARIA FATECANA
+
+        <img src=@{StaticR img_produto_jpg}>
+        
         <ul>
             <li> 
-                <a href=@{ProdutoR}>
-                    CADASTRO
+                <a href=@{EntrarR}>
+                    LOGIN
 
-            <li>
-                <a href=@{ListProdR}>
-                    LISTAR
-        
-            $maybe email <- sess
-                <li>
-                    <div>
-                        #{email}
-                        <form method=post action=@{SairR}>
-                            <input type="submit" value="Sair">
-            $nothing
-                <li>
-                    <a href=@{EntrarR}>
-                        ENTRE
+
     |]
